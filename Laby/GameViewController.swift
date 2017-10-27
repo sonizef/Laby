3c73694efa344c7e3f1db21e449cf5d3137fe19c
Joris ZEFIRINI <sonizef@gmail.com>
2017-10-27 17:06:57 +0200

    [CLEAN PROJECT]

--- Laby/GameViewController.swift
+++ Laby/GameViewController.swift
@@ -43,7 +43,7 @@ class GameViewController: UIViewController {
     }
 
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
